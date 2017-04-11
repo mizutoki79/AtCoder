9947f5f8e5ffc612d0f3976bb1f826aaa1affc48
mizutoki79 <a.p9xelic5@gmail.com>
2017-04-11 19:23:12 +0900

    ABC058_D問題のTLE解消 2重ループ->ループ2回に分割

--- ABC/ABC058/ABC058_D_井井井###.cs
+++ ABC/ABC058/ABC058_D_井井井###.cs
@@ -14,35 +14,27 @@ class Program
         string[] inputs = Console.ReadLine().Split(' ');
         int n = int.Parse(inputs[0]);
         int m = int.Parse(inputs[1]);
-        long[] x = Console.ReadLine().Split(' ')
-                        .Select(long.Parse)
+        int[] x = Console.ReadLine().Split(' ')
+                        .Select(int.Parse)
                         .OrderBy(elem => elem)
                         .ToArray();
-        long[] y = Console.ReadLine().Split(' ')
-                        .Select(long.Parse)
+        int[] y = Console.ReadLine().Split(' ')
+                        .Select(int.Parse)
                         .OrderBy(elem => elem)
                         .ToArray();
 
-        long totalArea = 0;
+        long totalArea = 0, areaX = 0, areaY = 0;
         for (int i = 1; i < n; i++)
         {
-            for (int j = 1; j < m; j++)
-            {
-                long area = 0;
-                //Console.Error.WriteLine("i: {0}, j:{1}", i, j);
-                //Console.Error.WriteLine("x[i] - x[i - 1] = {0} - {1} = {2}", x[i], x[i - 1], x[i] - x[i - 1]);
-                //Console.Error.WriteLine("y[j] - y[j - 1] = {0} - {1} = {2}", y[j], y[j - 1], y[j] - y[j - 1]);
-                area = (x[i] - x[i - 1]) * (y[j] - y[j - 1]) % Divisor;
-                //Console.Error.WriteLine("area = {0}", area);
-                area = area * i * (n - i) % Divisor;
-                //Console.Error.WriteLine(" * {0}", i * (n - i));
-                area = area * j * (m - j) % Divisor;
-                //Console.Error.WriteLine(" * {0}", j * (m - j));
-                //Console.Error.WriteLine("totalArea = {0} + {1} = ", totalArea, area);
-                totalArea = (totalArea + area) % Divisor;
-                //Console.Error.WriteLine("{0}", totalArea);
-            }
+            areaX += (long)(x[i] - x[i - 1]) * i % Divisor * (n - i) % Divisor;
+            areaX %= Divisor;
         }
+        for (int i = 1; i < m; i++)
+        {
+            areaY += (long)(y[i] - y[i - 1]) * i % Divisor * (m - i) % Divisor;
+            areaY %= Divisor;
+        }
+        totalArea = areaX * areaY % Divisor;
         
         Console.WriteLine(totalArea);
     }
