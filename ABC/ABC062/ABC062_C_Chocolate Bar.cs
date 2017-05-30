a3730dde2d813854646d768a1819f5829748c960
mizutoki79 <a.p9xelic5@gmail.com>
2017-05-30 23:03:29 +0900

    ABC062_C を更新 繰り返す処理をメソッドにまとめてスッキリさせた。また、切る位置を2分の1から3分の1から始めるようにして定数倍高速化 実行速度にして10倍くらい速くなった

--- ABC/ABC062/ABC062_C_Chocolate Bar.cs
+++ ABC/ABC062/ABC062_C_Chocolate Bar.cs
@@ -11,64 +11,51 @@ class Program
     static void Main()
     {
         string[] inputs = Console.ReadLine().Split(' ');
-        long H = long.Parse(inputs[0]);
-        long W = long.Parse(inputs[1]);
+        int H = int.Parse(inputs[0]);
+        int W = int.Parse(inputs[1]);
         long result = 0;
-        if(!(H % 3 == 0 || W % 3 == 0))
+        if (!(H % 3 == 0 || W % 3 == 0))
         {
-            if(H > W)
-            {
-                long tmp = H;
-                H = W;
-                W = tmp;
-            }
-            long separatePoint = W / 2;
-            long[] areas = new long[3];
-            long areaGap = long.MaxValue;
-            int i = 1;
-            while (separatePoint > 0)
-            {
-                areas[0] = H * separatePoint;
-                long H2 = H / 2, W2 = W - separatePoint;
-                areas[1] = H2 * W2;
-                areas[2] = (H - H2) * W2;
-                Console.Error.WriteLine("1-{6} - H1: {0}, W1: {1}, H2: {2}, W2: {3}, H3: {4}, W3: {5}", H, separatePoint, H2, W2, H - H2, W2, i++);
-                long compareAreaGap = areas.Max() - areas.Min();
-                Console.Error.WriteLine("area1: {0}, area2: {1}, area3: {2}, gap: {3}", areas[0], areas[1], areas[2], compareAreaGap);
-                if(areaGap > compareAreaGap)
-                {
-                    areaGap = compareAreaGap;
-                    separatePoint--;
-                    continue;
-                }
-                else break;
-            }
-            long tmp2 = H;
-            H = W;
-            W = tmp2;
-            separatePoint = W / 2;
-            long areaGap2 = long.MaxValue;
-            i = 1;
-            while (separatePoint > 0)
-            {
-                areas[0] = H * separatePoint;
-                long H2 = H / 2, W2 = W - separatePoint;
-                areas[1] = H2 * W2;
-                areas[2] = (H - H2) * W2;
-                Console.Error.WriteLine("2-{6} - H1: {0}, W1: {1}, H2: {2}, W2: {3}, H3: {4}, W3: {5}", H, separatePoint, H2, W2, H - H2, W2, i++);
-                long compareAreaGap = areas.Max() - areas.Min();
-                Console.Error.WriteLine("area1: {0}, area2: {1}, area3: {2}, gap: {3}", areas[0], areas[1], areas[2], compareAreaGap);
-                if(areaGap2 > compareAreaGap)
-                {
-                    areaGap2 = compareAreaGap;
-                    separatePoint--;
-                    continue;
-                }
-                else break;
-            }
-            result = Math.Min(areaGap, areaGap2);
-            result = result > Math.Min(H, W) ? Math.Min(H, W) : result;
+            result = Math.Min(SearchMinimumDifference(H, W), SearchMinimumDifference(W, H));
+            result = Math.Min(result, Math.Min(H, W));
         }
         Console.WriteLine(result);
+    }
+
+    static long SearchMinimumDifference(int H, int W)
+    {
+        long separatePoint = W / 3;
+        long originalSeparatePoint = separatePoint;
+        long difference = long.MaxValue;
+        bool isIncrease = true;
+        do
+        {
+            long newDifference = CalculateDifference(H, W, separatePoint);
+            Console.Error.WriteLine(": {0}", newDifference);
+            if (difference > newDifference)
+            {
+                difference = newDifference;
+                if (isIncrease) separatePoint++;
+                else separatePoint--;
+            }
+            else if (isIncrease)
+            {
+                separatePoint = originalSeparatePoint;
+                isIncrease = false;
+            }
+            else break;
+        } while (separatePoint > 0 && separatePoint <= W / 2);
+        return difference;
+    }
+
+    static long CalculateDifference(int H, int W, long separatePoint)
+    {
+        long[] areas = new long[3];
+        areas[0] = H * separatePoint;
+        long H2 = H / 2, W2 = W - separatePoint;
+        areas[1] = H2 * W2;
+        areas[2] = (H - H2) * W2;
+        Console.Error.WriteLine(string.Join(" ", areas));
+        return areas.Max() - areas.Min();
     }
 }
