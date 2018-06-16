ff0aceedbf2b49b506ef923d02eb6d3f1381e0dc
mizutoki79 <a.p9xelic5@gmail.com>
2018-06-17 00:06:11 +0900

    ABC100

---
+++ abc/ABC100/ABC100-B-Ringo'sFavoriteNumbers.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AtCoder.ABC100.B
+{
+    class Program
+    {
+        static void Main ()
+        {
+            var sc = new Scanner ();
+            var d = sc.Nextint ();
+            var n = sc.Nextint ();
+            if (n != 100) Console.WriteLine ((int) Math.Pow (100, d) * n);
+            else Console.WriteLine ((int) Math.Pow (100, d) * (n + 1));
+        }
+    }
+    class Scanner
+    {
+        string[] s;
+        long i;
+
+        char[] cs = new char[] { ' ' };
+
+        public Scanner ()
+        {
+            s = new string[0];
+            i = 0;
+        }
+
+        public string Next ()
+        {
+            if (i < s.Length) return s[i++];
+            string st = Console.ReadLine ();
+            while (st == "") st = Console.ReadLine ();
+            s = st.Split (cs, StringSplitOptions.RemoveEmptyEntries);
+            if (s.Length == 0) return Next ();
+            i = 0;
+            return s[i++];
+        }
+
+        public int Nextint ()
+        {
+            return int.Parse (Next ());
+        }
+        public int[] Arrayint (int N, int add = 0)
+        {
+            int[] Array = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                Array[i] = Nextint () + add;
+            }
+            return Array;
+        }
+
+        public long NextLong ()
+        {
+            return long.Parse (Next ());
+        }
+
+        public long[] ArrayLong (int N, long add = 0)
+        {
+            long[] Array = new long[N];
+            for (long i = 0; i < N; i++)
+            {
+                Array[i] = NextLong () + add;
+            }
+            return Array;
+        }
+
+        public double NextDouble ()
+        {
+            return double.Parse (Next ());
+        }
+
+        public double[] ArrayDouble (long N, double add = 0)
+        {
+            double[] Array = new double[N];
+            for (long i = 0; i < N; i++)
+            {
+                Array[i] = NextDouble () + add;
+            }
+            return Array;
+        }
+
+        public decimal NextDecimal ()
+        {
+            return decimal.Parse (Next ());
+        }
+
+        public decimal[] ArrayDecimal (long N, decimal add = 0)
+        {
+            decimal[] Array = new decimal[N];
+            for (long i = 0; i < N; i++)
+            {
+                Array[i] = NextDecimal ();
+            }
+            return Array;
+        }
+    }
+}
