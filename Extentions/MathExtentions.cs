b17b519161a69e3ab7144860e1f3c31a9a793b3f
mizutoki79 <a.p9xelic5@gmail.com>
2018-07-15 02:06:50 +0900

    Add a GCD method

--- Extentions/MathExtentions.cs
+++ Extentions/MathExtentions.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 static class MathExtensions
 {
-    public static long Factorial (long n)
+    public static long Factorial(long n)
     {
         long fact = n;
         for (long i = n - 1; i >= 1; i--)
@@ -17,12 +17,23 @@ static class MathExtensions
         return fact;
     }
 
-    static long Combination (long n, long r)
+    public static long Combination(long n, long r)
     {
         long molecule = 1;
         for (long i = 0; i < r; i++) molecule *= n - i;
         long denominator = 1;
         for (long i = 1; i <= r; i++) denominator *= i;
         return molecule / denominator;
+    }
+
+    public static long Gcd(long a, long b)
+    {
+        var r = Math.Min(a, b);
+        while (a != b && r != 0)
+        {
+            if (a > b) r = a %= b;
+            else r = b %= a;
+        }
+        return Math.Max(a, b);
     }
 }
