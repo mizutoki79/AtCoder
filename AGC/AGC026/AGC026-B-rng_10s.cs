6f349a6aca215f0b797acb4851024e62540e8e41
mizutoki79 <a.p9xelic5@gmail.com>
2018-07-14 23:34:41 +0900

    AGC026 B but TLE

---
+++ AGC/AGC026/AGC026-B-rng_10s.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AtCoder.AGC026.B
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sc = new Scanner();
+            var T = sc.Nextint();
+            var queries = Enumerable.Range(0, T).Select(i => new { A = sc.NextLong(), B = sc.NextLong(), C = sc.NextLong(), D = sc.NextLong() });
+            foreach (var query in queries)
+            {
+                var next = query.A;
+                var memo = new HashSet<long>();
+                var isLoop = false;
+                while (true)
+                {
+                    if (next >= query.B) next = next - (query.B * (next / query.B));
+                    else break;
+                    if (memo.Contains(next))
+                    {
+                        isLoop = true;
+                        break;
+                    }
+                    memo.Add(next);
+                    if (next <= query.C) next += query.D;
+                }
+                Console.WriteLine(isLoop ? "Yes" : "No");
+            }
+        }
+    }
+    class Scanner
+    {
+        string[] s;
+        long i;
+
+        char[] cs = new char[] { ' ' };
+
+        public Scanner()
+        {
+            s = new string[0];
+            i = 0;
+        }
+
+        public string Next()
+        {
+            if (i < s.Length) return s[i++];
+            string st = Console.ReadLine();
+            while (st == "") st = Console.ReadLine();
+            s = st.Split(cs, StringSplitOptions.RemoveEmptyEntries);
+            if (s.Length == 0) return Next();
+            i = 0;
+            return s[i++];
+        }
+
+        public int Nextint()
+        {
+            return int.Parse(Next());
+        }
+        public int[] Arrayint(int N, int add = 0)
+        {
+            int[] Array = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                Array[i] = Nextint() + add;
+            }
+            return Array;
+        }
+
+        public long NextLong()
+        {
+            return long.Parse(Next());
+        }
+
+        public long[] ArrayLong(int N, long add = 0)
+        {
+            long[] Array = new long[N];
+            for (long i = 0; i < N; i++)
+            {
+                Array[i] = NextLong() + add;
+            }
+            return Array;
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
+        }
+
+        public double[] ArrayDouble(long N, double add = 0)
+        {
+            double[] Array = new double[N];
+            for (long i = 0; i < N; i++)
+            {
+                Array[i] = NextDouble() + add;
+            }
+            return Array;
+        }
+
+        public decimal NextDecimal()
+        {
+            return decimal.Parse(Next());
+        }
+
+        public decimal[] ArrayDecimal(long N, decimal add = 0)
+        {
+            decimal[] Array = new decimal[N];
+            for (long i = 0; i < N; i++)
+            {
+                Array[i] = NextDecimal();
+            }
+            return Array;
+        }
+    }
+}
