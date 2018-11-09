f2e3028e6db8ff0510f787856e41581ec2cd6e9e
mizutoki_ <a.p9xelic5@gmail.com>
2018-11-09 23:54:37 +0900

    Add ABC113-C

---
+++ ABC/ABC113/ABC113-C-ID.rs
@@ -0,0 +1,78 @@
+use std::collections::*;
+use std::vec::Vec;
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input!{
+        n: usize,
+        m: usize,
+        py: [(usize1, usize); m],
+    }
+    let mut city_infos = vec![BTreeMap::new(); n];
+
+    for (i, &(p, y)) in py.iter().enumerate() {
+        city_infos[p].insert(y, i);
+    }
+    let mut result = vec![String::new(); m];
+    for (p, order) in city_infos.iter().enumerate() {
+        let mut count = 0;
+        for idx in order.values() {
+            count += 1;
+            result[*idx] = format!("{:06}{:06}", p + 1, count);
+        }
+    }
+    for id in result {
+        println!("{}", id);
+    }
+}
