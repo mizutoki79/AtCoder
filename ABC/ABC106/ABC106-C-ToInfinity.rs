8e0da97eb574428615d13e93a2a820e923ea8925
mizutoki_ <a.p9xelic5@gmail.com>
2018-11-11 22:02:26 +0900

    Add ABC106-C in Rust

---
+++ ABC/ABC106/ABC106-C-ToInfinity.rs
@@ -0,0 +1,78 @@
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::vec::Vec;
+
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input!{
+        s: chars,
+        k: usize,
+    }
+    let first_over_two = s
+        .iter()
+        .enumerate()
+        .skip_while(|(_i, x)| x.to_string().parse::<i32>().unwrap() == 1)
+        .next();
+    if let Some(first_over_two) = first_over_two {
+        if first_over_two.0 < k {
+            println!("{}", first_over_two.1);
+            return;
+        }
+    }
+    println!("1");
+}
