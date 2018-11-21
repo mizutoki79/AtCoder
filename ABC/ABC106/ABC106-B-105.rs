1d73f20d5b86754193bbd036172a0d60e82104d4
mizutoki_ <a.p9xelic5@gmail.com>
2018-11-21 23:01:04 +0900

    Add ABC106-B

---
+++ ABC/ABC106/ABC106-B-105.rs
@@ -0,0 +1,89 @@
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::vec::Vec;
+
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input!{
+        n: usize,
+    }
+    if n < 105 {
+        println!("0");
+        return;
+    } else if n == 105 {
+        println!("1");
+        return;
+    } else {
+        let mut result = 1;
+        for dividend in 107..n + 1 {
+            if dividend % 2 != 1 {
+                continue;
+            }
+            let mut cnt = 2;
+            for divisor in 2..dividend {
+                if dividend % divisor == 0 {
+                    cnt += 1;
+                }
+            }
+            if cnt == 8 {
+                result += 1;
+            }
+        }
+        println!("{}", result)
+    }
+}
